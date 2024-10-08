766a41be39d56fe13e979a51c5a036f18f9fe1fd
klips-4 <klips-4@yandex.ru>
2024-10-08 17:04:11 +0500

    добавлены: Календарь, Статистика, Панель навигации

---
+++ src/components/Clients/ClientsPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+  <h2>Clients</h2>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Kanban/KanbanPage.vue
@@ -0,0 +1,12 @@
+<template>
+<h2 class="text-white">dsf</h2>
+</template>
+<script setup>
+
+
+
+</script>
+
+<style lang="scss">
+
+</style>
